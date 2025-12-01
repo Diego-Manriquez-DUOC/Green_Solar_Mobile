89b0aac5026617683fa1298a3c545c4a5aca04b4
DebugggMax <max.olguin@duocuc.cl>
2025-12-01 12:49:30 -0300

    borre todas estos archivos por la refactorizacion

--- app/src/main/java/com/green_solar/gs_app/data/remote/dto/ProductDto.kt
+++
@@ -1,8 +0,0 @@
-package com.green_solar.gs_app.data.remote.dto
-
-data class ProductDto (
-    val id: Long,
-    val name: String,
-    val price: Double,
-    val description: String
-)
--- app/src/main/java/com/green_solar/gs_app/ui/components/product/ProductViewModel.java
+++
@@ -1,5 +0,0 @@
-package com.green_solar.gs_app.ui.components.product;
-
-public class ProductViewModel {
-
-}
